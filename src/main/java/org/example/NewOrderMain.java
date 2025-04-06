7504296c8d65ea3d374da52eb88c2222f80378c8
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-04-06 14:26:05 -0300

    refactor: serializandos dados com GSON, alterando key e values dos dispatchers

--- src/main/java/org/example/NewOrderMain.java
+++ src/main/java/org/example/NewOrderMain.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -15,17 +16,20 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
 
-        try(var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 5; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + "1234,100,1213213123123";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        try(var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try(var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 5; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
 
+                }
             }
-
         }
 
     }
